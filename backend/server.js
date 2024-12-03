1285fde4317ac0ba4bdba9bc0976a9e06d0f7388
Vipul Tyagi <vipultyagi52444@gmail.com>
2024-12-03 19:13:08 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,9 @@ DbCon()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    credentials: true,
-    origin:"http://localhost:5173"
+    
+    origin:"http://localhost:5173",
+    credentials: true
 }));
 
 app.use('/api/auth',AuthRoutes)
